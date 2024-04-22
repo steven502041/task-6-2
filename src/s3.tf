0cdf1a8798b6928d4fac26f28a4fba3aef50e568
steven502041 <git@github.com:steven502041/appwork.git>
2024-04-22 18:02:15 +0800

    new

--- src/s3.tf
+++ src/s3.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "task-6-2"
     key            = "terraform/terraform.tfstate"
     region         = "ap-northeast-1"
-    dynamodb_table = "task-6-2"
     encrypt        = true
   }
 }
